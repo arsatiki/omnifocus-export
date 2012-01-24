3b035553a93a6cf448e974e0bd470d7b0758aac1
Antti Rasinen <antti.rasinen@codento.com>
2012-01-24 17:27:28 +0200

    Add more OO Tree just for the fun of it

--- read.py
+++ read.py
@@ -2,33 +2,43 @@ import csv
 import sys
 from collections import defaultdict
 
-class Tree(object):
-    def __init__(self, data):
-        self.data = data
-        self.children = []
-        path = self['Task ID']
-        self.level = len(path.split('.')) if path else 0
-
+class Node(object):
     def add(self, child):
         self.children.append(child)
-
-    def __getitem__(self, key):
-        return self.data.get(key, '')
 
     def __str__(self):
         children = '\n'.join(map(str, self.children))
         return self.level * '  ' + self['Task'] + children
 
+class Root(Node):
+    level = 0
+    def __init__(self):
+        self.children = []
+    
+    def add(self, child):
+        self.children.append(child)
+    
+    def __getitem__(self, key):
+        return ''
+
+class Tree(Node):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.level = len(self['Task ID'].split('.'))
+
+    def __getitem__(self, key):
+        return self.data[key]
+
 def make_tree(seq):
-    root = Tree({})
-    stack = [root]
+    stack = [Root()]
     
     for item in map(Tree, seq):
         while item.level <= stack[-1].level:
             stack.pop()
         stack[-1].add(item)
     
-    return root
+    return stack[0]
 
 def read(filename):
     with open(filename) as f:
